a7c6adb91e860b2301157b364c0eced19fd299c7
1109s1 <no159753@naver.com>
2020-12-28 16:42:15 +0900

    20201228 html수업내용

---
+++ c#aray/C_sooup/C_sooup/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_sooup
+{
+    class Program
+    {
+        private static int[] numbers;
+
+        private static void Main()
+        {
+            int min = int.MaxValue;
+            int max = int.MinValue;
+
+            numbers = new int[int.Parse(Console.ReadLine())];
+
+            string[] args = Console.ReadLine().Split(' ');
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int arg = int.Parse(args[i]);
+
+                if (arg < min) min = arg;
+                if (max < arg) max = arg;
+            }
+
+            Console.Write(min.ToString() + " " + max.ToString());
+        }
+    }
+}
